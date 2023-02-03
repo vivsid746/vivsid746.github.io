7138a0bee67b4264751ca2d7cfc6f2610a26133f
Vivek Singhal <100491144+vivsid746@users.noreply.github.com>
2023-02-03 19:38:09 +0530

    some change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,8 +8,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/vivsid746/vivsid746.github.io/master/src/Assets/viveksinghal_citech_23.pdf";
+// const resumeLink =
+//   "https://raw.githubusercontent.com/vivsid746/vivsid746.github.io/master/src/Assets/viveksinghal_citech_23.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
